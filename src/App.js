b489bfcd0049a1cd4da7a1299c8e878994f3c614
Prathap Kumaravel <50357476+Prathap0127@users.noreply.github.com>
2022-11-10 13:39:58 +0530

    final

--- src/App.js
+++ src/App.js
@@ -2,15 +2,87 @@ import './App.css';
 import Card from './components/Card.js';
 
 function App() {
+  let data=[
+    {
+    plan:"FREE",
+    price:0,
+    user:"Single User",
+    userEnabler:true,
+    storage:"5GB Storage",
+    storageEnabler:true,
+    publicProjects:"Unlimited Public Projects",
+    publicProjectsEnabler:true,
+    communityAccess:"Community Access",
+    communityAccessEnabler:true,
+    privateProjects:"Unlimited Private Projects",
+    privateProjectsEnabler:false,
+    phoneSupport:"Dedicated Phone Support",
+    phoneSupportEnabler:false,
+    subdomain:"Free Subdomain",
+    subdomainEnabler:false,
+    reports:"Monthly Status Reports",
+    reportsEnabler:false,
+  },
+  {
+    plan:"PLUS",
+    price:9,
+    user:"5 User",
+    userEnabler:true,
+    storage:"50GB Storage",
+    storageEnabler:true,
+    publicProjects:"Unlimited Public Projects",
+    publicProjectsEnabler:true,
+    communityAccess:"Community Access",
+    communityAccessEnabler:true,
+    privateProjects:"Unlimited Private Projects",
+    privateProjectsEnabler:true,
+    phoneSupport:"Dedicated Phone Support",
+    phoneSupportEnabler:true,
+    subdomain:"Free Subdomain",
+    subdomainEnabler:true,
+    reports:"Monthly Status Reports",
+    reportsEnabler:false,
+
+  },
+  {
+    plan:"PRO",
+    price:49,
+    user:"Unlimited User",
+    userEnabler:true,
+    storage:"150GB Storage",
+    storageEnabler:true,
+    publicProjects:"Unlimited Public Projects",
+    publicProjectsEnabler:true,
+    communityAccess:"Community Access",
+    communityAccessEnabler:true,
+    privateProjects:"Unlimited Private Projects",
+    privateProjectsEnabler:true,
+    phoneSupport:"Dedicated Phone Support",
+    phoneSupportEnabler:true,
+    subdomain:"Free Subdomain",
+    subdomainEnabler:true,
+    reports:"Monthly Status Reports",
+    reportsEnabler:true,
+  }
+]
+
+
   return <>
   
 <section className="pricing py-5">
   <div className="container">
     <div className="row">
-     <Card/>
-     <Card/>
-     <Card/>
-     
+      {/* one way of doing with array index */}
+     {/* <Card card={data[0]}/>
+     <Card card={data[1]}/>
+     <Card card={data[2]}/>
+      */}
+      {/* using map */}
+      {
+        data.map((e)=>{
+          return <Card card={e}/>
+        })
+      }
      
     </div>
   </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,23 @@
 import React from 'react'
 
-function Card() {
+function Card(props) {
+  console.log(props)
   return <>
     <div className="col-lg-4">
         <div className="card mb-5 mb-lg-0">
           <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">Free</h5>
-            <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
+            <h5 className="card-title text-muted text-uppercase text-center">{props.card.plan}</h5>
+            <h6 className="card-price text-center">${props.card.price}<span className="period">/month</span></h6>
             <hr/>
             <ul className="fa-ul">
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-              <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited
-                Private Projects</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated
-                Phone Support</li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain
-              </li>
-              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status
-                Reports</li>
+              <li className={props.card.userEnabler?"":"text-muted"}><span className="fa-li"><i className={props.card.userEnabler?"fas fa-check":"fas fa-times"}></i></span>{props.card.plan==="FREE"?<>{props.card.user}</>:<b>{props.card.user}</b>}</li>
+              <li className={props.card.storageEnabler?"":"text-muted"}><span className="fa-li"><i className={props.card.storageEnabler?"fas fa-check":"fas fa-times"}></i></span>{props.card.storage}</li>
+              <li className={props.card.publicProjectsEnabler?"":"text-muted"}><span className="fa-li"><i className={props.card.publicProjectsEnabler?"fas fa-check":"fas fa-times"}></i></span>{props.card.publicProjects}</li>
+              <li className={props.card.communityAccessEnabler?"":"text-muted"}><span className="fa-li"><i className={props.card.communityAccessEnabler?"fas fa-check":"fas fa-times"}></i></span>{props.card.communityAccess}</li>
+              <li className={props.card.privateProjectsEnabler?"":"text-muted"}><span className="fa-li"><i className={props.card.privateProjectsEnabler?"fas fa-check":"fas fa-times"}></i></span>{props.card.privateProjects}</li>
+              <li className={props.card.phoneSupportEnabler?"":"text-muted"}><span className="fa-li"><i className={props.card.phoneSupportEnabler?"fas fa-check":"fas fa-times"}></i></span>{props.card.phoneSupport}</li>
+              <li className={props.card.subdomainEnabler?"":"text-muted"}><span className="fa-li"><i className={props.card.subdomainEnabler?"fas fa-check":"fas fa-times"}></i></span>{props.card.plan==="PRO"?<><b>Unlimited </b>{props.card.subdomain}</>:<>{props.card.subdomain}</>}</li>
+              <li className={props.card.reportsEnabler?"":"text-muted"}><span className="fa-li"><i className={props.card.reportsEnabler?"fas fa-check":"fas fa-times"}></i></span>{props.card.reports}</li>
             </ul>
             <div className="d-grid">
               <a href="javascript(void)" className="btn btn-primary text-uppercase">Button</a>
